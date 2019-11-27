94a09dd0d84708ef20470bb639dcb6683c5c8af6
Chloé Keating <chloe.m.keating@gmail.com>
2019-11-27 11:36:03 -0800

    new py ideas

--- 2016/one.py
+++ 2016/one.py
@@ -7,16 +7,21 @@
 
 
 filename = "input03.txt"
-lines = file_object.readlines()
+triangles = file_object.readlines()
 #https://stackabuse.com/read-a-file-line-by-line-in-python/
+accum = 0 
+
+#https://www.reddit.com/r/adventofcode/comments/5g80ck/2016_day_3_solutions/daq7xok/
+#triangle values into int?
 
 
-if string[] = side[1] + side [2] > side[3] :
-or if string[] = side[2] + side [3] > side[1] :
-or if string[] = side[1] + side [3]] > side[2] :
-
+if side[] = side[1] + side [2] > side[3] 
+and side[] = side[2] + side [3] > side[1] 
+and side[] = side[1] + side [3]] > side[2] 
 return accum + 1 , string +1 
 
 else 
 return accum, string + 1
 
+print ("Number of valid triangles:")
+print validTri
