e89fdb6bffd29f316f48e2eb7c00be24a59d032d
Chloé Keating <chloe.m.keating@gmail.com>
2019-11-26 17:12:42 -0800

    day 03 first draft py

---
+++ 2016/one.c
@@ -0,0 +1,5 @@
+// Advent of Code 2016 Day 03 Squares with Three Sides
+
+// take file input03.txt and analyse each string, a valid triangle is one where
+// "the sum of any two sides is greater than the third side"
+
---
+++ 2016/one.py
@@ -0,0 +1,22 @@
+# Advent of Code 2016 Day 03 Squares with Three Sides
+
+# take file input03.txt and analyse each string, a valid triangle is one where
+# "the sum of any two sides is greater than the third side"
+
+#which libraries should I include?
+
+
+filename = "input03.txt"
+lines = file_object.readlines()
+#https://stackabuse.com/read-a-file-line-by-line-in-python/
+
+
+if string[] = side[1] + side [2] > side[3] :
+or if string[] = side[2] + side [3] > side[1] :
+or if string[] = side[1] + side [3]] > side[2] :
+
+return accum + 1 , string +1 
+
+else 
+return accum, string + 1
+
