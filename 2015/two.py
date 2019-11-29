00f13a35281494244fb7d982a676e504a7a9b9e7
Chloé Keating <chloe.m.keating@gmail.com>
2019-11-28 16:53:18 -0800

    new peices for day 1 part two

---
+++ 2015/two.py
@@ -0,0 +1,20 @@
+# Advent of Code 2015 day 01 
+# PART TWO 
+
+#puzzle file name = input01.txt
+
+#https://github.com/jjhelmus/adventofcode/blob/master/day01.py simple but understandable, I hope...
+
+#Setting the scene
+floor = 0 
+position = 1
+
+
+with open ("input01.txt") as fo:
+    directions = fo.read()
+    floor = directions.count( "(" ) - directions.count( ")" )
+
+if floor = -1
+    break
+
+print ("It took you long enough", position)
