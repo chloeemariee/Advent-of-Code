dcd9b4d5c87219b9da26dad203da3ac4743ea118
Chloé Keating <chloe.m.keating@gmail.com>
2019-11-28 18:09:46 -0800

    back to IF, fixed with enumerate

--- 2015/two.py
+++ 2015/two.py
@@ -3,17 +3,21 @@
 
 #puzzle file name = input01.txt
 
-#https://github.com/jjhelmus/adventofcode/blob/master/day01.py their part two is a  
-#little confusing but the elements mesh so let's see what we can make of it
+#https://github.com/ChrisPenner/Advent-Of-Code-Polyglot/blob/master/2015/python/01/part2.py
+#a way to fix my original if version
 
 #Setting the scene becuase now we need to keep track
 floor = 0 
 position = 1
 
 #The Count
-with open ("input01.txt") as fo: # the 'with' means I don't need to close
-    directions = fo.read()
-    for position:
-        floor += (1 if directions == "(" , position += 1 else -1 , position += 1)
-        if floor < 0:
-            print ("the position", position)
+with open ("input01.txt") as f: # the 'with' means I don't need to close
+    for position, char in enumerate(f.read(), 1):
+        if char == "(":
+            floor += 1 
+        elif char == ")" :
+            floor -= 1 
+        if floor == -1:
+            break;
+
+print ("It took you long enough", position)
