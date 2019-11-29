b29135969965f06e8d22c90b4951ecb2f60b14d6
Chloé Keating <chloe.m.keating@gmail.com>
2019-11-28 16:23:49 -0800

    first try

--- 2015/one.py
+++ 2015/one.py
@@ -1,1 +1,12 @@
-# open ( means go up close ) means go down
+# Advent of Code 2015 day 01
+#open ( means go up close ) means go down
+
+#puzzle file name = input01.txt
+
+#https://github.com/jjhelmus/adventofcode/blob/master/day01.py simple but understandable, I hope...
+
+open ("input01.txt")
+    directions = f.read()
+    floor = directions.count( "(" ) - directions.count( ")" )
+
+print("Go to floor:", floor)
