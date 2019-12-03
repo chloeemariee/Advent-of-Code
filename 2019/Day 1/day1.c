a30e11889db6817e4ecf49da01ed1760348b09cf
Chloé Keating <chloe.m.keating@gmail.com>
2019-12-02 17:45:29 -0800

    Starting Day 1

---
+++ 2019/Day 1/day1.c
@@ -0,0 +1,35 @@
+// Advent of Code 2019
+// Day 1: The Tyranny of the Rocket Equation
+
+// "the fuel required for a module =  its mass, divide by three, round down, and subtract 2"
+
+//Fuel Counter-Upper = how much total fuel needed
+//Calculate the fuel for each module (input.txt) and sum them 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+//File IO
+fp = fopen("input.txt","r");
+
+module = m
+fuel_for_module = m / 3 - 2 // rounding after / 3?
+
+int main {
+
+
+    
+}
+
+//keep the fuel for each module in an array, you'll need them seperate for part 2
+//gotta use tail recursion
+// recursion
+//    base case
+//    recursive case
+
+if fuel <= 0 
+    return total;
+
+// typecasting
