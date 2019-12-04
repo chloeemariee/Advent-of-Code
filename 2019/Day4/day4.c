76f05d1590d6f1476b32985ede7400089679710d
Chloé Keating <chloe.m.keating@gmail.com>
2019-12-04 12:03:21 -0800

    can't figure out condition C and D

--- 2019/Day4/day4.c
+++ 2019/Day4/day4.c
@@ -27,10 +27,10 @@ int upperbound = 767253; //last real option 666666
 // Define variables
 
 int valid_passwords = 0 ; // running total
-int six_digits = 0;           // A  
-int in_range = 0;         // B  
-int valid_pairs = 0;      // C  
-int pairs = 0;
+int six_digits = 0;          // A  
+int in_range = 0;            // B  
+int valid_pairs = 0;         // C  
+int pairs = 0;  
 int max_pairs = 1620 ;
 /* how many numbers are there between 111111 - 999999 that have at least one pair?
 9 * 4 = 36      digits * (digits - pair) = 1 spot
@@ -59,36 +59,38 @@ int main () {
 
     // number of passwords that satisfy condition A
     six_digits = (999999 -= 111112 += 1);             //Why can't I add two things here and ...
-    printf ("digits", six_digits);
+    printf ("digits = %d", six_digits);
 
 
     // number of passwords that satisfies condition B
     in_range = (upperbound -= lowerbound) += 1;       // ...here???? I could get rid of A, B works to do the same
-    printf ("in range", in_range);
+    printf ("in range = %d", in_range);
 
 
     // number of passwords that satisfies condition C
     valid_pairs;
-        if (pairs >= lowerbound)
-            max_pairs= 0;
-        if (pairs <= upperbound)
-            max_pairs = 0;
+        if (pairs <= lowerbound)
+            valid_pairs += 0;
+        if (pairs >= upperbound)
+            valid_pairs += 0;
         else (max_pairs - pairs);
         // idk what I'm doing here, should I find where all the pairs are in the array?
-    printf ("pairs", valid_pairs);
+    printf ("pairs = %d", valid_pairs);
 
 
     // number of passwords that satisfies condition D
     size_matters;
-        if (all_passwords[], ([0]<=[1]<=[2]<=[3]<=[4]<=[5]))
-            increment += 1;
-        else
-            increment;
-    printf ("size", size_matters);
+        if (all_passwords[] == ([0]<=[1]<=[2]<=[3]<=[4]<=[5])) {
+            size_matters += 1;
+        }
+        else {
+            size_matters += 0;
+        }
+    printf ("size = %d", size_matters);
 
 
 
     valid_passwords = ((((valid_passwords + six_digits )- in_range) - valid_pairs ) - size_matters); 
-    printf ("number of valid passwords =", valid_passwords);
+    printf ("number of valid passwords = %d", valid_passwords);
     return 0;
 }
