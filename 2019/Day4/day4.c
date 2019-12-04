194aea3bc5211f8adb35f27fa278e8104fcac7f8
Chloé Keating <chloe.m.keating@gmail.com>
2019-12-03 21:43:48 -0800

    start modularising conditions, commit before I lose them

--- 2019/Day4/day4.c
+++ 2019/Day4/day4.c
@@ -1,20 +1,37 @@
 /* Advent of Code 2019 Day 4 
 Secure Container
 
-Looking for a password:
-    6 digits
-    in range of "input.txt" (273,025-767,253)
-    there's at least one number pair e.g. 1_22_479
-    from left to right numbers can only increse
+Looking for a password: 
+    A. 6 digits
+    B. in range of "input.txt" (273,025-767,253)
+    C. there's at least one number pair e.g. 1_22_479
+    D. from left to right numbers can only increase
 
 How many different password combinations exist in your range?
 */
 
-// 1. Read/Process the number range
+/* 1. Read/Process the number range
+(to generalise, ask people who read my code to manually change lower and upper bounds?)
+*/
 int lowerbound = 273025;
 int upperbound = 767253;
 
 // 2. Modularise password conditions 
+
+int valid_passwords; // a running total we'll switch into each module
+
+int in_range () { // number of passwords that satisfy A and B
+    (upperbound -= lowerbound) += 1;
+    valid_passwords += in_range
+}
+
+= (upperbound -= lowerbound) += 1; 
+int number_pairs (){ //something like this, so you can still change the range
+    if >= lowerbound
+        = 0;
+    if <= upperbound
+        = 0;
+}
 
 // 3. Main? Count how many passwords there are?
 
