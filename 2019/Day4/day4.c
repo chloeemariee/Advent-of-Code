919115959afbd49a02be7cbd38ead39915843fa6
Chloé Keating <chloe.m.keating@gmail.com>
2019-12-03 23:54:25 -0800

    put everything in main, new different errors replace the old ones

--- 2019/Day4/day4.c
+++ 2019/Day4/day4.c
@@ -14,52 +14,14 @@ How many different password combinations exist in your range?
 #include<stdlib.h>
 #include<math.h>
 
-/* 1. Read/Process the number range
+/* Read/Process the number range
 (If you're not me, you can add your own input range by changing these bounds)
 */
-int lowerbound = 273025;
-int upperbound = 767253;
+int lowerbound = 273025; //first real option 333333
+int upperbound = 767253; //last real option 666666
 
 
-// 2. Modularise password conditions 
-
-int valid_passwords = 0 ; // a running total to switch into each module for the return?
-
-int digits () { // number of passwords that satisfies A
-    999999 -= 111111 += 1;
-
-    return 0;
-}
-
-int in_range () { // number of passwords that satisfies B
-    (upperbound -= lowerbound) += 1;
-
-    return 0;
-}
-
-/* how many numbers are there between 111111 - 999999 that have at least one pair?
-    9 * 4 = 36      digits * (digits - pair) = 1 spot
-    36 * 5 = 180    1 spot * number of spots = 1 pair 
-    180 * 9 = 1620  1 pair * number of pairs = total pairs
-*/
-int pairs = 1620; 
-
-int valid_pairs (){ // number of passwords that satisfies C
-    
-    if(>= lowerbound)
-        = 0;
-    if(<= upperbound)
-        = 0;
-    
-    return 0;
-}
-
-int size_matters () { // number of passwords that satisfies D
-
-    return 0;
-}
-
-// 3. Main? Count how many passwords there are?
+// Main
 
 int main () {
     
@@ -67,14 +29,46 @@ int main () {
     for(int i = 111111; i <1000000; i++)
     all_passwords[i] = i; 
 
+    // Password conditions 
+
+    int valid_passwords = 0 ; // a running total to switch into each module for the return
+
+    int digits () { // number of passwords that satisfies condition A
+        digits = (999999 -= 111112 += 1);
+
+        return 0;
+    }
+
+    int in_range () { // number of passwords that satisfies condition B
+        (upperbound -= lowerbound) += 1;
+
+        return 0;
+    }   
+
+    /* how many numbers are there between 111111 - 999999 that have at least one pair?
+    9 * 4 = 36      digits * (digits - pair) = 1 spot
+    36 * 5 = 180    1 spot * number of spots = 1 pair 
+    180 * 9 = 1620  1 pair * number of pairs = total pairs
+    */
+    int pairs = 1620; 
+
+    int valid_pairs (){ // number of passwords that satisfies condition C
+    
+        if (something >= lowerbound)
+            something_else = 0;
+        if (something <= upperbound)
+            something_else = 0;
+        else (a); 
+
+        return 0;
+    }  
+
+    int size_matters () { // number of passwords that satisfies condition D
+
+        return 0;
+    }
+
+    valid_passwords = ((((valid_passwords + digits )- in_range) ) - valid_pairs ) - size_matters); 
 
     return 0;
 }
-
-/*
-valid_passwords += digits;
-    valid_passwords -= in_range;
-    valid_passwords -= valid_pairs;
-    valid_passwords -= size_matters;
-
-*/
