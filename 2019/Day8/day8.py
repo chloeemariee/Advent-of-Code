8541a7a4dde2ac9611359eee73ae62b5ded2f7bb
Chloé Keating <chloe.m.keating@gmail.com>
2019-12-09 11:58:45 -0800

    get rid of old print check

--- 2019/Day8/day8.py
+++ 2019/Day8/day8.py
@@ -15,11 +15,6 @@ pixels = len(all_layers)
 number_of_layers = int(pixels/image_size)
 pixels_per_layer = int(pixels/number_of_layers)
 
-# Do they work?
-print(pixels)           # = 15000
-print(number_of_layers) # = 100
-print(pixels_per_layer) # = 150
-
 # Find the right layer
 # how to https://www.reddit.com/r/adventofcode/comments/e7pkmt/2019_day_8_solutions/fa6re9v/
 
@@ -28,11 +23,11 @@ def count_pixels(num, layer):
 def count_zeroes(layer):
     return count_pixels (0, layer)
 
-def key_layer ():
+def special_layer ():
     pixels_per_layer
     the_layer = min(pixels_per_layer, key=count_zeroes)
     ones = count_num(1, the_layer)
     twos = count_num(2, the_layer)
     return ones * twos
 
-print(f"final answer:", key_layer)
+print(f"final answer:", special_layer())
