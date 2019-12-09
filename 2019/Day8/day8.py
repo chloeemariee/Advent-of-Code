f300d5e3ecc86e71d4e187a2c2c09da8c61eadc3
Chloé Keating <chloe.m.keating@gmail.com>
2019-12-09 11:55:32 -0800

    an answer, not the right one

--- 2019/Day8/day8.py
+++ 2019/Day8/day8.py
@@ -1,3 +1,6 @@
+import sys
+import itertools
+
 # Advent of Code 2019 Day 8
 # Space Image Format
 
@@ -17,3 +20,19 @@ print(pixels)           # = 15000
 print(number_of_layers) # = 100
 print(pixels_per_layer) # = 150
 
+# Find the right layer
+# how to https://www.reddit.com/r/adventofcode/comments/e7pkmt/2019_day_8_solutions/fa6re9v/
+
+def count_pixels(num, layer):
+    return len([px for px in itertools.chain(*layer) if px == num])
+def count_zeroes(layer):
+    return count_pixels (0, layer)
+
+def key_layer ():
+    pixels_per_layer
+    the_layer = min(pixels_per_layer, key=count_zeroes)
+    ones = count_num(1, the_layer)
+    twos = count_num(2, the_layer)
+    return ones * twos
+
+print(f"final answer:", key_layer)
