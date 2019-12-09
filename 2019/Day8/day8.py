958235d4e0c97595f15ae22c165ab4e76aa99cda
Chloé Keating <chloe.m.keating@gmail.com>
2019-12-09 11:34:40 -0800

    print to check

--- 2019/Day8/day8.py
+++ 2019/Day8/day8.py
@@ -1,10 +1,19 @@
 # Advent of Code 2019 Day 8
 # Space Image Format
 
-# 25 x 6 = size of image
+# Open and strip file
 
-# Open File
-with open("input.txt", "r") as fo:
-  line = fo.readline()
+all_layers =  [int(i) for i in open('input.txt', 'r').read().strip()] # more efficient file IO https://www.reddit.com/r/adventofcode/comments/e7pkmt/2019_day_8_solutions/fa8hr35/
 
-pixels =  [int(i) for i in open('input.txt', 'r').read().strip()] # more efficient file IO https://www.reddit.com/r/adventofcode/comments/e7pkmt/2019_day_8_solutions/fa8hr35/
+# Define variables
+# how to define variables these https://paste.ofcode.org/MYzf7jtL5nBpw8DJFzmqVv
+image_size = 25*6   # = 150
+pixels = len(all_layers)
+number_of_layers = int(pixels/image_size)
+pixels_per_layer = int(pixels/number_of_layers)
+
+# Do they work?
+print(pixels)           # = 15000
+print(number_of_layers) # = 100
+print(pixels_per_layer) # = 150
+
