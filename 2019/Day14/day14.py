2c0c5b5c61426e9ba9cbe4b64af3800e2be00965
Chloé Keating <chloe.m.keating@gmail.com>
2020-01-03 10:31:19 -0800

    day14 plan

---
+++ 2019/Day14/day14.py
@@ -0,0 +1,18 @@
+# Advent of Code 2019 Day 14
+# Space Stiochiometry
+
+# planning stage
+
+
+# file IO split into a list of formulas
+formulas = [int(i) for i in open('input.txt', 'r').read().strip()]
+
+elements split by , = [ ] + [ ]
+transform ==> into =
+
+Sort formulas 
+    ore
+    regular forumulas
+    fuel 
+
+# Part two, count and quantify
