29bec1d4f09b0cf1950e241b380ed1dc8ffdd5de
Chloé Keating <chloe.m.keating@gmail.com>
2020-01-03 05:21:10 -0800

    new plan of attack

--- 2019/Day6/Day6.py
+++ 2019/Day6/Day6.py
@@ -1,15 +1,27 @@
+# Advent of Code 2019 Day 6
+# Universal Orbit Map
 
 
+# for direct orbits
+
+# file IO
 with open("input.txt") as f:
     for line in f:
     orbits = [line.rstrip()] #https://www.reddit.com/r/adventofcode/comments/e6tyva/2019_day_6_solutions/f9wefu1/
     planet = orbit.split(")")[0]
     satellite = orbit.split(")")[1]
 
+# make a tree. How do you make a tree? build??
+build tree 
+    where planet = root
+    and   satellite = branch
+    reccur  
 
-#can't i create a set of all the objects in space...? all the unique ones are the number of connections...cheating but will work
+# count direct and indirect orbits
+direct = pairs
+indirect = run through the whole tree
 
-number_of_orbits = set(orbits)
+total_orbits = direct + indirect
 
-print(len(orbits))
-
+# does this work?
+print(total_orbits)
