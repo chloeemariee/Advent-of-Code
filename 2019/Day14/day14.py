043036f747f4193489dc8a3f83fa18db51fd7123
Chloé Keating <chloe.m.keating@gmail.com>
2020-01-06 15:21:04 -0800

    update day14 w new ideas

--- 2019/Day14/day14.py
+++ 2019/Day14/day14.py
@@ -1,6 +1,6 @@
 # Advent of Code 2019 Day 14
 # Space Stiochiometry
-
+ 
 import sys, math
 
 # file IO split into a list of formulas
@@ -9,11 +9,21 @@ formulas = [int(i) for i in open('input.txt', 'r').read().strip()]
 #elements split by , = [ ] + [ ]
 #transform ==> into =
 
-line.split(' => ')
+line.split(' => ');
 
-Sort formulas 
-    ore
+sort_formulas; 
+    find."ORE"
     regular forumulas
-    fuel 
+    find."FUEL" 
+
+def amounts 
+    remember who you are
 
 # Part two, count and quantify
+
+def make_fuel (amounts, formulas) {
+    start with ore
+    what do you need to make fuel
+}
+
+print("how much ore for fuel", steps);
