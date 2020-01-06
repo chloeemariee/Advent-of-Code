58e5c898f679d54bb1026083d624cda5eea5d21b
Chloé Keating <chloe.m.keating@gmail.com>
2020-01-05 23:07:00 -0800

    day 24 plan

---
+++ 2019/Day24/day24.py
@@ -0,0 +1,32 @@
+# Advent of Code 2019 Day 24
+# Planet of Discord
+
+# planning
+
+with open("input.txt") as f:
+    for line in f:
+
+#Part 1 define the game parameters
+
+def alive
+    if 
+    else
+
+def dead
+    if 
+    else
+
+
+
+
+# part 2 remember the configurations
+
+#increasing powers of two
+def biodiversity 
+    if alive
+        * []2
+
+def number_of_moves
+    enumerate
+
+printf( "number of moves %d", number_of_moves);
