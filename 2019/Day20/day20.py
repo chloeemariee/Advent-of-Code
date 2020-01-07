ac25047e83afb3b7f9b3e56c0d816c9ee4574389
Chloé Keating <chloe.m.keating@gmail.com>
2020-01-06 17:59:04 -0800

    day20 plan

--- 2019/Day20/day20.py
+++ 2019/Day20/day20.py
@@ -1,3 +1,38 @@
 # Advent of Code 2019 Day 20
 # Donut Maze
 
+#planning
+
+import math
+import os
+import pathlib
+import sys
+
+# biggest challenge will be to parse the maze
+
+with open("input.txt") as f:
+    for line in f:
+
+#read the maze
+def portals
+
+def starting_point
+    center to AA
+
+def last_gate
+    finding_ZZ
+
+# play the game
+def move
+    current_position (starting_point)
+    hunt_gates (unlocked)
+    hunt_keys (stored_keys)
+    where to go next
+
+def hunt_keys
+    if theres a key close by how far is it
+    stored_keys
+
+def hunt_gates
+    if theres a key close by how far is it
+    open_gates
